149d9caf2e2b72cf3ed172fafb8a1da3665a9772
elenejavakhishvili638 <elenejavaxishvili@yahoo.com>
2023-04-01 09:48:46 -0700

    style: change image container preview

--- resources/views/movies/main-page.blade.php
+++ resources/views/movies/main-page.blade.php
@@ -2,9 +2,16 @@
     {{-- <div class="grid w-full">  </div> --}}
     @include('movies._translate-buttons')
     <div class="text-center flex flex-col items-center">
-        <div alt="im" class="rounded-lg border h-386 w-700">image</div>
-        <a href="#" class="text-5xl mt-65 mb-114 text-white underline">Movie title</a>
-        <p class="text-5xl text-white">"Movie Quote"</p>
+        @if (!$movie->quotes || !count($movie->quotes))
+        @else
+            <div alt="im" class="rounded-lg border h-386 w-700">image</div>
+        @endif
+        <a href="#" class="text-5xl mt-65 mb-114 text-white underline">{{ $movie->title }}</a>
+        @if (!$movie->quotes || !count($movie->quotes))
+            <p class="text-5xl text-white">No quote here</p>
+        @else
+            <p class="text-5xl text-white">"{{ $movie->quotes[0]->body }}"</p>
+        @endif
     </div>
 
 </x-layout>
